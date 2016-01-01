c242e05a0cf09c21f8f50e0c2dd472298f518f6c
David Warburton <david.warburton@gmail.com>
2016-01-01 15:40:56 -0800

    parse request started

--- HttpServerDriver/HttpRequest.cpp
+++ HttpServerDriver/HttpRequest.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <deque>
 
-#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/iter_find.hpp>
+#include <boost/algorithm/string/finder.hpp>
 
 #include "HttpRequest.hpp"
 
@@ -16,8 +18,32 @@ namespace Naive
             std::string requeststr(data.begin(), data.begin() + data_size);
             std::cout << "Got Request: " << requeststr << std::endl;
 
-            //parse
-            std::istringstream ss(requeststr, std::ios_base::in);
+            std::deque<std::string> v;
+            iter_split(v, requeststr, boost::algorithm::first_finder("\r\n"));
+            
+            std::string http_line = v.front();
+            v.pop_front();
+            while (v.size() > 0)
+            {
+                std::string x = v.front();
+                v.pop_front();
+                if (x == "")
+                {
+                    break;
+                }
+                std::cout << "header: " << x << std::endl;
+            }
+            //remaining strings in v are the request body
+            
+            std::stringstream bodyss;
+            for (size_t i = 0; i < v.size(); ++i)
+            {
+                if (i != 0)
+                    bodyss << "\r\n";
+                bodyss << v[i];
+            }
+            std::string body = bodyss.str();
+            
         }
 
 
@@ -27,6 +53,27 @@ namespace Naive
         bool Request::keep_alive()
         {
             return false;
+        }
+        std::string Request::get_header(std::string name)
+        {
+            if (m_headers.find(name) != m_headers.end())
+            {
+                return m_headers[name];
+            }
+            return "";
+            
+        }
+        std::string Request::get_operation()
+        {
+            return m_operation;
+        }
+        std::string Request::get_url()
+        {
+            return m_url;
+        }
+        std::string Request::get_http_version()
+        {
+            return m_http_version;
         }
     } //namespace Http
 } //namespace Naive
--- HttpServerDriver/HttpRequest.hpp
+++ HttpServerDriver/HttpRequest.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <map>
+
 namespace Naive
 {
     namespace Http
@@ -11,6 +13,16 @@ namespace Naive
             ~Request();
 
             bool keep_alive();
+            std::string get_header(std::string);
+            std::string get_operation();
+            std::string get_url();
+            std::string get_http_version();
+
+        private:
+            std::map<std::string, std::string> m_headers;
+            std::string m_operation;  // one of GET, POST etc.
+            std::string m_url;
+            std::string m_http_version;
         };
     } //namespace Http
 } //namespace Naive
